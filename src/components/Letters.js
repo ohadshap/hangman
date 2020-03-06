5095ea8dfd9fe6da354e65a6e89d7dfad8baf063
Ohad Shapira <ohadshap@gmail.com>
2020-03-06 16:55:01 +0200

    part one completed with all letters

---
+++ src/components/Letters.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Letter from './Letter';
+
+class Letters extends Component {
+    render() {
+        
+        return (<div>
+            <Letter />
+        </div>)
+    }
+}
+export default Letters
